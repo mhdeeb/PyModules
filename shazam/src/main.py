a43f9324e0a7255964a6ed8f62b536b69593d09d
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2022-12-27 19:15:10 +0200

    Format shazam/main.py and remove /main.py

--- shazam/src/main.py
+++ shazam/src/main.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import sys
 import time
-from typing import Union
+from typing import Union, AsyncGenerator, Any
 
 from shazamio import *
 from pydub import AudioSegment
@@ -32,7 +32,7 @@ def parse_args(args: list[str]) -> list[dict[str, Union[str, int]]]:
     return output
 
 
-async def recognize():
+async def recognize() -> AsyncGenerator[dict[str, Any], None]:
     args = parse_args(sys.argv[1:])
     for arg in args:
         if 'url' in arg:
@@ -52,48 +52,54 @@ async def recognize():
         else:
             yield await Shazam().recognize_song(arg['path'])
         if arg['path'].startswith('.temp_'):
-            to_delete.append(arg['path'])
+            recognize.to_delete.append(arg['path'])
 
 
-to_delete = []
+recognize.to_delete = []
 
 
-async def main():
+def serialize(result: dict[str, Any]) -> tuple[str, dict[str, Union[str, int]]]:
+    song = {}
+    string = "\n"
+    if 'matches' in result and result['matches']:
+        stamp = ', '.join(map(str, set(
+            '{:0>2}:{:0>2}'.format(int(match['offset'] // 60), int(match['offset'] % 60)) for match in
+            result['matches']
+            if 'offset' in match)))
+        if 'track' in result:
+            track = result['track']
+            if 'title' in track:
+                song['title'] = track['title']
+                string += f"\nTitle: {track['title']}"
+            if 'subtitle' in track:
+                song['artist'] = track['subtitle']
+                string += f"\nArtist: {track['subtitle']}"
+        if stamp:
+            song['time'] = stamp
+            string += f"\nTime: {stamp}"
+        if 'location' in result and 'accuracy' in result['location']:
+            song['accuracy'] = result['location']['accuracy'] * 10000
+            string += f"\nAccuracy: {song['accuracy']}%"
+    return string, song
+
+
+def save(data: tuple[str, dict[str, str | int]], file) -> None:
+    if not hasattr(save, "total"):
+        save.total = []
+    if not data[0].isspace():
+        file.seek(0)
+        save.total.append(data[1])
+        json.dump(save.total, file, indent=4, ensure_ascii=False)
+        print(data[0])
+    else:
+        print("\n\nTrack not found.")
+
+
+async def main() -> None:
     with open("results.json", "w", encoding="UTF-8") as file:
-        song = {}
-        total = []
-        async for result in recognize():
-            string = "\n"
-            if 'matches' in result and result['matches']:
-                stamp = ', '.join(map(str, set(
-                    '{:0>2}:{:0>2}'.format(int(match['offset'] // 60), int(match['offset'] % 60)) for match in
-                    result['matches']
-                    if 'offset' in match)))
-                if 'track' in result:
-                    track = result['track']
-                    if 'title' in track:
-                        song['title'] = track['title']
-                        string += f"\nTitle: {track['title']}"
-                    if 'subtitle' in track:
-                        song['artist'] = track['subtitle']
-                        string += f"\nArtist: {track['subtitle']}"
-                if stamp:
-                    song['time'] = stamp
-                    string += f"\nTime: {stamp}"
-                if 'location' in result and 'accuracy' in result['location']:
-                    song['accuracy'] = result['location']['accuracy'] * 10000
-                    string += f"\nAccuracy: {song['accuracy']}%"
-            if not string.isspace():
-                file.seek(0)
-                total.append(song)
-                song = {}
-                json.dump(total, file, indent=4, ensure_ascii=False)
-                print(string)
-            else:
-                print("\n\nTrack not found.")
-        for path in to_delete:
-            os.remove(path)
-        print("\nDone!")
+        [save(serialize(result), file) async for result in recognize()]
+    [os.remove(path) for path in recognize.to_delete]
+    print("\nDone!")
 
 
 asyncio.run(main())
